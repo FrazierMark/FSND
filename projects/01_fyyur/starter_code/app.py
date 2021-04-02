d1fd5fd692436d7fe1b6d9e584ce0353975194e5
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-04-02 12:24:51 -0500

    Completing artist, venue, and shows tables and implementing via Flask-Migrate.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -10,6 +10,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
+from flask_migrate import Migrate
 from flask_wtf import Form
 from forms import *
 #----------------------------------------------------------------------------#
@@ -20,12 +21,16 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database
+# TODO: connect to a local postgresql database 
+#Using psql, connect to fyyur db---- Done.
 
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
+
+
 
 class Venue(db.Model):
     __tablename__ = 'Venue'
@@ -40,6 +45,10 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # DONE, initialized flask db migration via...
+    # python3 -m flask db init
+    # python3 -m flask db migrate
+    # python3 -m flask db upgrade
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -54,6 +63,7 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    # Complete. Artist table now in local db.
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
