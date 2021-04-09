ec74ef30273e3cdbc55f461962b8e8eae9672beb
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-04-09 06:21:27 -0500

    Finished Fyyur app. Implemented edit functions and delete functions.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -239,18 +239,35 @@ def create_venue_submission():
   
   # TODO-DONE: on unsuccessful db insert, flash an error instead. -DONE
   
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
+  # TODO-DONE: Complete this endpoint for taking a venue_id, and using
   # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+  venue = Venue.query.get(venue_id)
+  error = False
+
+  try:
+    db.session.delete(venue)
+    db.session.commit()
+    flash('Venue ' + {venue.name} + ' was successfully deleted!')
+  except():
+    error = True
+    db.session.rollback()
+    flash('Venue ' + {venue.name} + ' could not be deleted.')
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+    flash('Venue table updated!')
+  if error:
+    abort(500)
+  else:
+    return render_template('pages/home.html')
+
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+ 
 
 
 #  Artists
@@ -361,33 +378,21 @@ def edit_artist_submission(artist_id):
   
   # re-writing information to those columns grabbed
   try:
-    artist.name = request.form['name']
-    artist.city=request.form['city']
-    artist.state=request.form['state']
-    artist.phone=request.form['phone']
-    artist.genres=request.form['genres']
-    artist.image_link=request.form['image_link']
-    artist.facebook_link=request.form['facebook_link']
-    artist.website_link=request.form['website_link']
-    artist.seeking_description=request.form['seeking_description']
-    if request.form['seeking_venue'] == 'y':
+    form = ArtistForm(request.form)
+    
+    artist.name =form.name.data
+    artist.city=form.city.data
+    artist.state=form.state.data
+    artist.phone=form.phone.data
+    artist.genres=form.genres.data
+    artist.image_link=form.image_link.data
+    artist.facebook_link=form.facebook_link.data
+    artist.website_link=form.website_link.data
+    artist.seeking_description=form.seeking_description.data
+    if form.seeking_venue.data == 'y':
       artist.seeking_venue=True
     else:
-      artist.seeking_venue= 'Null'
-    
-    # artist.name =form.name
-    # artist.city=form.city
-    # artist.state=form.state
-    # artist.phone=form.phone
-    # artist.genres=form.genres
-    # artist.image_link=form.image_link
-    # artist.facebook_link=form.facebook_link
-    # artist.website_link=form.website_link
-    # artist.seeking_description=form.seeking_description
-    # if form.seeking_venue == 'y':
-    #   artist.seeking_venue=True
-    # else:
-    #   artist.seeking_venue=False
+      artist.seeking_venue=False
     
     db.session.add(artist)
     db.session.commit()
@@ -408,28 +413,65 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get(venue_id)
+  
+  if venue:
+    form.name.data = venue.name.strip("\,()."),
+    form.genres.data = venue.genres,
+    form.city.data = venue.city,
+    form.state.data = venue.state,
+    form.address.data = venue.address,
+    form.phone.data = venue.phone,
+    form.website_link.data = venue.website_link,
+    form.facebook_link.data = venue.facebook_link,
+    form.seeking_talent.data = venue.seeking_talent,
+    form.seeking_description.data = venue.seeking_description,
+    form.image_link.data = venue.image_link
+
+  # TODO--DONE: populate form with values from venue with ID <venue_id>
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
   # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
+  venue = Venue.query.get(venue_id)
+  error = False
+  
+  # re-writing information to those columns grabbed
+  try:
+    form = VenueForm(request.form)
+    
+    venue.name =form.name.data.strip("\,().")
+    venue.city=form.city.data
+    venue.state=form.state.data
+    venue.address=form.address.data
+    venue.phone=form.phone.data
+    venue.genres=form.genres.data
+    venue.image_link=form.image_link.data
+    venue.facebook_link=form.facebook_link.data
+    venue.website_link=form.website_link.data
+    venue.seeking_description=form.seeking_description.data
+    if form.seeking_talent.data == 'y':
+      venue.seeking_talent=True
+    else:
+      venue.seeking_talent=False
+    
+    db.session.add(venue)
+    db.session.commit()
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+  except():
+    error = True
+    db.session.rollback()
+    flash('Venue ' + request.form['name'] + ' could not be updated.')
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+    flash('Venue table updated!')
+  if error:
+    abort(500)
+  else:
+    return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
 #  ----------------------------------------------------------------
