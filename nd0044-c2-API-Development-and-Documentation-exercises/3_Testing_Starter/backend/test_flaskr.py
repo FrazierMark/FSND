d23ae46cef087bde2cce6c24abd57f7b490d7c44
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-04-20 12:50:22 -0500

    Practice, Testing in Flask.

--- nd0044-c2-API-Development-and-Documentation-exercises/3_Testing_Starter/backend/test_flaskr.py
+++ nd0044-c2-API-Development-and-Documentation-exercises/3_Testing_Starter/backend/test_flaskr.py
@@ -14,7 +14,7 @@ class BookTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "bookshelf_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}:{}@{}/{}".format('frazier', 'postgres','localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         self.new_book = {
@@ -34,13 +34,98 @@ class BookTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-# @TODO: Write at least two tests for each endpoint - one each for success and error behavior.
-#        You can feel free to write additional tests for nuanced functionality,
-#        Such as adding a book without a rating, etc. 
-#        Since there are four routes currently, you should have at least eight tests. 
-# Optional: Update the book information in setUp to make the test database your own! 
+    def testRetrieveBooks(self):
+        "Testing if books can be retrieved from db."
+        res = self.client().get('/books')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_books'])
+        self.assertTrue(len(data['books']))
+
+    def testRetrieveBooksFail(self):
+        res = self.client().get('/books?page=1000', json={'rating': 1})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+
+
+
+
+
+    def testUpdateBooks(self):
+        "Testing if we can update books endpoint"
+        res = self.client().patch('/books/5', json={'rating': 1})
+        data = json.loads(res.data)
+        book = Book.query.filter(Book.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(book.format()['rating'], 1)
+
+    def testUpdateBooksFail(self):
+        res = self.client().patch('/books/5')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'bad request')
+
+
+
+
+
+
+    def testDeleteBooks(self):
+        "Testing if we can delete books endpoint"
+        res = self.client().delete('books/5')
+        data = json.loads(res.data)
+        book = Book.query.filter(Book.id == 5).one_or_none()
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['deleted'], 1)
+        self.assertTrue(data['total_books'])
+        self.assertTrue(len(data['books']))
+        self.assertEqual(book, None)
+
+    def testDeleteBooksFail(self):
+        res = self.client().delete('/books/1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+        
+
+
+
+
+        
+    def test_create_new_book(self):
+        "Testing if we can create a new book"
+        res = self.client().post('/books', json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['created'])
+        self.assertTrue(len(data['books']))
+
+    def testCreateNewBookFail(self):
+        res = self.client().post('/books/55', json=self.new_book)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 405)
+        self.assertEqual(res.data['success'], False)
+        self.assertFalse(res.data['message'], 'method not allowed')
 
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
+
