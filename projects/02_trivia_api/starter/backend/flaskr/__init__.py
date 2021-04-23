3f33deddfa8eb76a7ba508a366ed6647d34031a8
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-04-23 07:30:56 -0500

    Set up CORS and headers for project 2.

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -13,19 +13,36 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   
+  
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+  DONE: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 
   '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
+  DONE: Use the after_request decorator to set Access-Control-Allow
   '''
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS')
+    return response
+
+  
 
   '''
   @TODO: 
   Create an endpoint to handle GET requests 
   for all available categories.
   '''
+  @app.route('/categories')
+  def get_categories():
+    "Retrieve all categories of trivia questions."
+    categories = Category.query.all()
+    return jsonify(categories)
+
+
 
 
   '''
