dbd6a5e1a02e096b68f1c6a86873e5477d7e98d0
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-07-28 18:37:38 -0700

    Getting header to reroute to my pages.

--- projects/capstone/final/frontend/src/App.js
+++ projects/capstone/final/frontend/src/App.js
@@ -1,17 +1,20 @@
-import React, { useState, useEffect, Component } from 'react';
+import React, { Component } from 'react';
 import Header from "./components/Header";
 import './App.scss'
-import { Canvas, useFrame } from '@react-three/fiber';
-import CameraModel from './components/CameraModel';
-import FilmModel from './components/FilmModel';
-import LensModel from './components/CameraModel';
-import Terrain from './components/Terrain';
 import LandingPage from './components/LandingPage';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 
 const App = () => {
   return (
+    <>
+    <Header />
     <LandingPage/>
+    </>
   )
 }
 
