97f623363e24e1e3cec3ff47499097fc057cf9e3
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-07-28 18:19:17 -0700

    Added glitch effect. Got side tracked.

--- projects/capstone/final/frontend/src/App.js
+++ projects/capstone/final/frontend/src/App.js
@@ -6,30 +6,12 @@ import CameraModel from './components/CameraModel';
 import FilmModel from './components/FilmModel';
 import LensModel from './components/CameraModel';
 import Terrain from './components/Terrain';
+import LandingPage from './components/LandingPage';
 
 
 const App = () => {
   return (
-    <>
-    <Header />
-    <Canvas
-    colorManagement
-    shadowMap
-    camera={{ position: [0, 0, 60], fov: 50 }}>
-    
-      
-    <ambientLight intensity={0.5} />
-    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-    <pointLight position={[-10, -10, -10]} />
-
-    <CameraModel position={[-2.2, 0, 40]} />
-    <FilmModel position={[-0.2, 0, 40]} />
-    <LensModel position={[2.2, 0, 40]} />
-
-    <Terrain/>
-
-  </Canvas>
-  </>
+    <LandingPage/>
   )
 }
 
---
+++ projects/capstone/final/frontend/src/components/LandingPage.js
@@ -0,0 +1,42 @@
+import { Canvas, useFrame } from '@react-three/fiber';
+import GlitchText from "./GlitchText";
+import Header from "./Header";
+import CameraModel from './CameraModel';
+import FilmModel from './FilmModel';
+import LensModel from './CameraModel';
+import Terrain from './Terrain';
+
+
+const LandingPage = () => {
+  return (
+    <>
+    <Header />
+
+    <div className="App">
+      <h1>
+        <GlitchText>Welcome</GlitchText>
+      </h1>
+    </div>
+
+    <Canvas
+    colorManagement
+    shadowMap
+    camera={{ position: [0, 0, 60], fov: 50 }}>
+    
+      
+    <ambientLight intensity={0.5} />
+    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+    <pointLight position={[-10, -10, -10]} />
+
+    <CameraModel position={[-2.2, 0, 40]} />
+    <FilmModel position={[-0.2, 0, 40]} />
+    <LensModel position={[2.2, 0, 40]} />
+
+    <Terrain/>
+
+  </Canvas>
+  </>
+  )
+}
+
+export default LandingPage;
