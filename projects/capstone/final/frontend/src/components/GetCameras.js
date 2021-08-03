85e1c6ab44dd0aed95dca6d489fa8cdbe3326183
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-08-02 21:41:22 -0700

    Troubleshooting GET response.

--- projects/capstone/final/frontend/src/components/GetCameras.js
+++ projects/capstone/final/frontend/src/components/GetCameras.js
@@ -1,54 +1,81 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import DataTable from './DataTable'
-import { CompressedPixelFormat } from 'three';
+
 
 export default class Cameras extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { camerasCollection: [] };
-    }
+        
+        this.state = {
+          kind: '',
+          data: []
+        };
+      }
 
     componentDidMount() {
         axios.get('http://127.0.0.1:5000/CameraPage')
-        .then(res => {
-            this.setState({ usersCollection: res.data });
-            })
+        .then(({ data })=> {
+            console.log(data)
+            this.setState({ 
+            kind: data.kind, 
+            data: data.data.children
+          });
+        })
+        .catch((err)=> {})
+    }
+        
+//     render() {
+//       const child = this.state.data.map((el, index) => {
+//         return <div key={index}>
+//           <p>Title - { el.data.cameras }</p>
+//         </div>
+//       });
+  
+//       return <div>
+//         <p>{ this.state.kind }</p>
+//         <div>{ child }</div>
+//       </div>;
+//     }
+//   }
+
+
+//     renderTableHeader() {
+        
+//         let header = Object.keys(this.state.cameras[0])
+//         return header.map((key, index) => {
+//            return <th key={index}>{key.toUpperCase()}</th>
+//         })
+//      }
+
+    renderTableData() {
+        const { id, brand, model, sensor, mount } = 
+            this.state.data.map((data, i) => {
             
-            .catch(function (error) {
-                console.log(error);
-            })
+            return (
+                <tr key={id}>
+                    <td>{id}</td>
+                    <td>{brand}</td>
+                    <td>{model}</td>
+                    <td>{sensor}</td>
+                    <td>{mount}</td>
+                </tr>
+            )   
+        })
     }
 
-
-    dataTable() {
-        return this.state.camerasCollection.map((data, i) => {
-            return <DataTable obj={data} key={i} />;
-        });
-    }
-
-    render() {
-        return (
-            <div className="wrapper-users">
-                <div className="container">
-                    <table className="table table-striped table-dark">
-                        <thead className="thead-dark">
-                            <tr>
-                                <td>ID</td>
-                                <td>Brand</td>
-                                <td>Model</td>
-                                <td>Sensor</td>
-                                <td>Mount</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            
-                            {this.dataTable()}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//            <div>
+//               <h1 id='title'>React Dynamic Table</h1>
+//               <table id='cameras'>
+//                  <tbody>
+//                     <tr>{this.renderTableHeader()}</tr>
+//                     {this.renderTableData()}
+//                  </tbody>
+//               </table>
+//            </div>
+//         )
+//      }
+//   }
