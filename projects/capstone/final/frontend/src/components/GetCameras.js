b841044b0cab6537bb655ee33861db382b351f1a
FrazierMark <60164353+FrazierMark@users.noreply.github.com>
2021-08-05 09:36:18 -0700

    Getting Camera data to frontend, need to style a bit.

--- projects/capstone/final/frontend/src/components/GetCameras.js
+++ projects/capstone/final/frontend/src/components/GetCameras.js
@@ -1,18 +1,17 @@
 import React, { Component, useEffect, useState } from 'react';
 import axios from 'axios';
-import DataTable from './DataTable'
 
 
-const getCameras = () => {
-const [camera, setCamera] = useState([])
+const GetCameras = () => {
+    const [camera, setCamera] = useState([]);
 
     const getCameraData = async () => {
         try {
             const data = await axios.get('http://127.0.0.1:5000/CameraPage');
         console.log(data.data.cameras);
-        setCamera(data.data.cameras)
+        setCamera(data.data.cameras);
     } catch (e){
-        console.log(e)
+        console.log(e);
     }
 };
     
@@ -23,11 +22,11 @@ const [camera, setCamera] = useState([])
     <div className="Cameras">
         <h1> TEST </h1>
         {camera.map((item) => {
-            return <p>{item.name}</p>
+            return <p>{item.brand} - {item.model} - {item.sensor} </p>
         })}
     </div>
     );
 };
 
 
-export default getCameras;
+export default GetCameras;
